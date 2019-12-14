35a747fe30f631258e8ff458297a211eb19f47b3
kitagami <kitagami@artifactnoise.com>
2019-12-14 17:04:20 +0900

    add

--- time_check.py
+++ time_check.py
@@ -15,10 +15,12 @@ for i in range(10):
     cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８個(64bit分)の枠を指示
     (DATA, WKC) = cat.socket_read() #結果を読出し
     endsum = endsum + (time.time() - start)
+    if(tmax>(time.time() - start) tmax = (time.time() - start)
 end = endsum / i
 print ("平均　time:{0}".format( (end)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
+print ("最大:{0}".format( (tmax)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
-
+exit()
 endsum =0
 print("2回目の計測")
 time.sleep(2)
