4d87d003be633e64dfe5146de43a49371679805d
kitagami <kitagami@artifactnoise.com>
2019-12-14 16:54:37 +0900

    add

--- time_check.py
+++ time_check.py
@@ -17,7 +17,7 @@ print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".
 
 print(" ")
 ## 2回目の計測
-ADP = 0x0000 #1台目
+ADP = 0x0000-1 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
 start = time.time()     #実行時間のスタート計算
 cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８個(64bit分)の枠を指示
@@ -25,13 +25,11 @@ cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８�
 print ("time:{0}".format( (time.time() - start)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
-print(" ")
-print(" ")
 time.sleep(2)
 
 print(" ")
 ## 3回目の計測
-ADP = 0x0000 #1台目
+ADP = 0x0000-2 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
 start = time.time()     #実行時間のスタート計算
 cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８個(64bit分)の枠を指示
@@ -41,7 +39,7 @@ print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".
 
 print(" ")
 ## 4回目の計測
-ADP = 0x0000 #1台目
+ADP = 0x0000-3 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
 start = time.time()     #実行時間のスタート計算
 cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８個(64bit分)の枠を指示
