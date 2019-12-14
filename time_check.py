c0b56353d2f61ccc7efcba2b3ad942b59acf006c
kitagami <kitagami@artifactnoise.com>
2019-12-14 17:13:03 +0900

    add

--- time_check.py
+++ time_check.py
@@ -9,8 +9,8 @@ print("1回目の計測")
 endsum =0
 tmax = 0
 tmin = 1
-time.sleep(2)
-for i in range(10):
+time.sleep(1)
+for i in range(100):
     ADP = 0x0000 #1台目
     ADDR = 0x0E00 #コアレジスタのアドレス
     start = time.time()     #実行時間のスタート計算
@@ -25,13 +25,14 @@ print ("最大　time:{0}".format( (tmax)*1000) + "[msec]")     #実行時間の
 print ("最小　time:{0}".format( (tmin)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
-## 1回目の計測
+print(" ")
+## 2回目の計測
 print("2回目の計測")
 endsum =0
 tmax = 0
 tmin = 1
-time.sleep(2)
-for i in range(10):
+time.sleep(1)
+for i in range(100):
     ADP = 0x0000-1 #1台目
     ADDR = 0x0E00 #コアレジスタのアドレス
     start = time.time()     #実行時間のスタート計算
@@ -46,13 +47,14 @@ print ("最大　time:{0}".format( (tmax)*1000) + "[msec]")     #実行時間の
 print ("最小　time:{0}".format( (tmin)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
-## 1回目の計測
+print(" ")
+## 3回目の計測
 print("3回目の計測")
 endsum =0
 tmax = 0
 tmin = 1
-time.sleep(2)
-for i in range(10):
+time.sleep(1)
+for i in range(100):
     ADP = 0x0000-2 #1台目
     ADDR = 0x0E00 #コアレジスタのアドレス
     start = time.time()     #実行時間のスタート計算
@@ -67,13 +69,14 @@ print ("最大　time:{0}".format( (tmax)*1000) + "[msec]")     #実行時間の
 print ("最小　time:{0}".format( (tmin)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
+print(" ")
 ## 4回目の計測
 print("4回目の計測")
 endsum =0
 tmax = 0
 tmin = 1
-time.sleep(2)
-for i in range(10):
+time.sleep(1)
+for i in range(100):
     ADP = 0x0000-3 #1台目
     ADDR = 0x0E00 #コアレジスタのアドレス
     start = time.time()     #実行時間のスタート計算
