4ae06d4c7d06427b75b0f4619cf20592556bcd9c
kitagami <kitagami@artifactnoise.com>
2019-12-14 16:21:31 +0900

    add

--- time_check.py
+++ time_check.py
@@ -10,14 +10,14 @@ ADP = 0x0000 #1台目
 ADDR = 0x0138 #コアレジスタのアドレス
 data = [0]*1
 ## 1回目の計測
-data[0] = 0x10 | 0x0D
+data[0] = 0x10 | 0x00
 start = time.time()     #実行時間のスタート計算
 cat.APWR(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=data) #データ書き込み
 (DATA, WKC) = cat.socket_read() #結果を読出し
 #print("[0x{:04X}]= 0x{:02x}".format(ADDR, DATA[0])) #読み出したデータを表示する
 print ("time:{0}".format( (time.time() - start)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 ## 2回目の計測
-data[0] = 0x10 | 0x00
+data[0] = 0x10 | 0x0D
 start = time.time()     #実行時間のスタート計算
 cat.APWR(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=data) #データ書き込み
 (DATA, WKC) = cat.socket_read() #結果を読出し
@@ -30,7 +30,7 @@ ADP = 0x0000-1 #2台目
 ADDR = 0x0138 #コアレジスタのアドレス
 data = [0]*1
 ## 3回目の計測
-data[0] = 0x10 | 0x0D
+data[0] = 0x10 | 0x00
 start = time.time()     #実行時間のスタート計算
 cat.APWR(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=data) #データ書き込み
 (DATA, WKC) = cat.socket_read() #結果を読出し
@@ -38,7 +38,7 @@ cat.APWR(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=data) #データ書き込み
 print ("time:{0}".format( (time.time() - start)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 
 ## 4回目の計測
-data[0] = 0x10 | 0x00
+data[0] = 0x10 | 0x0D
 start = time.time()     #実行時間のスタート計算
 cat.APWR(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=data) #データ書き込み
 (DATA, WKC) = cat.socket_read() #結果を読出し
