77227ffe8f1c51495c4435a1231fcfb9a5cd1421
kitagami <kitagami@artifactnoise.com>
2019-12-14 16:53:35 +0900

    add

--- time_check.py
+++ time_check.py
@@ -3,18 +3,19 @@ import time #実行時間計測用のtimeライブラリ
 nic = "eth0" # ネットワークカードのアドレスを記載
 cat = MasterEtherCAT.MasterEtherCAT(nic)  
 
-
+print(" ")
 time.sleep(2)
 
+## 1回目の計測
 ADP = 0x0000 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
-## 1回目の計測
 start = time.time()     #実行時間のスタート計算
 cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８個(64bit分)の枠を指示
 (DATA, WKC) = cat.socket_read() #結果を読出し
 print ("time:{0}".format( (time.time() - start)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
+print(" ")
 ## 2回目の計測
 ADP = 0x0000 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
@@ -24,8 +25,11 @@ cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８�
 print ("time:{0}".format( (time.time() - start)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
+print(" ")
+print(" ")
 time.sleep(2)
 
+print(" ")
 ## 3回目の計測
 ADP = 0x0000 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
@@ -35,6 +39,7 @@ cat.APRD(IDX=0x00, ADP=ADP, ADO=ADDR, DATA=[0,0,0,0,0,0,0,0]) #DATAは０を８�
 print ("time:{0}".format( (time.time() - start)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 
+print(" ")
 ## 4回目の計測
 ADP = 0x0000 #1台目
 ADDR = 0x0E00 #コアレジスタのアドレス
