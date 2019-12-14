47315385811d291c5c4ce2e076edd205d4ac2bce
kitagami <kitagami@artifactnoise.com>
2019-12-14 17:10:04 +0900

    add

--- time_check.py
+++ time_check.py
@@ -7,6 +7,8 @@ print(" ")
 ## 1回目の計測
 endsum =0
 tmax = 0
+tmin = 1
+
 print("1回目の計測")
 time.sleep(2)
 for i in range(10):
@@ -17,9 +19,11 @@ for i in range(10):
     (DATA, WKC) = cat.socket_read() #結果を読出し
     endsum = endsum + (time.time() - start)
     tmax = (time.time() - start) if(tmax < (time.time() - start)) else tmax
+    tmin = (time.time() - start) if(tmin > (time.time() - start)) else tmin
 end = endsum / i
 print ("平均　time:{0}".format( (end)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
-print ("最大:{0}".format( (tmax)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
+print ("最大　time:{0}".format( (tmax)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
+print ("最小　time:{0}".format( (tmin)*1000) + "[msec]")     #実行時間のエンド計算と表示まで
 print("[0x{:04X}]= 0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x},0x{:02x}{:02x}".format(ADDR, DATA[7],DATA[6],DATA[5],DATA[4],DATA[3],DATA[2],DATA[1],DATA[0]))
 exit()
 endsum =0
