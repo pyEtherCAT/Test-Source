3bbeb3e4267e378b67afcc8181a04974b44334fd
kitagami <kitagami@artifactnoise.com>
2019-12-15 18:39:54 +0900

    add

--- steping_moter.py
+++ steping_moter.py
@@ -87,15 +87,15 @@ def main():
     try:
         while 1:
             # time.sleep(TIME)
-            for i in range(45*10):
+            for i in range(50*10):
                 for s in range(len(STEP)):
                     cat.ADP = 0x0000 - 0
                     EtherCAT_GPIO_Out(cat, STEP[s])
                     time.sleep(TIME)
 
             EtherCAT_GPIO_Out(cat, 0x0000)
-            time.sleep(5)
-            for i in range(200*10):
+            time.sleep(2)
+            for i in range(50*10):
                 for s in range(len(STEP)):
                     cat.ADP = 0x0000 - 0
                     EtherCAT_GPIO_Out(cat, STEP[s])
